2112bbf95dfc162a88e8e068bc5ad8afdd6aceae
ho0215 <ho021524@gmail.com>
2026-08-21 12:53:31 +0900

    Remove pymysql installation and version settings
    Removed pymysql configuration from settings.

--- config/settings.py
+++ config/settings.py
@@ -13,11 +13,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from pathlib import Path
-import pymysql
-pymysql.install_as_MySQLdb()
-pymysql.version_info = (2, 2, 4, "final", 0)
-pymysql.__version__ = "2.2.4"
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
