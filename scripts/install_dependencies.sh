7eea84b808a2b959cb0c36eb98813814ecf8a7bb
ho0215 <ho021524@gmail.com>
2026-08-21 16:24:23 +0900

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+# 에러가 나면 즉시 스크립트를 중단
+set -e
+
 cd /home/ec2-user/aniverse
 
-# 파이썬 가상환경 활성화 (ec2-user 경로에 맞춤)
+# 파이썬 가상환경 활성화
 source /home/ec2-user/venv/bin/activate
 
-# 필요한 패키지 업데이트
+# pip 및 기본 툴 업그레이드 (버전 충돌 방지)
+pip install --upgrade pip setuptools wheel
+
+# 필요한 패키지 설치
 pip install -r requirements.txt
 
 # DB 마이그레이션 적용 (RDS MariaDB)
 python manage.py migrate --noinput
 
-# 정적 파일 모으기 (EFS 마운트 경로로 자동 이동)
+# 정적 파일 모으기
 python manage.py collectstatic --noinput
