ecd2fbae69d8a77c6498bf07e7b3990c5f7d71a9
ho0215 <ho021524@gmail.com>
2026-08-21 12:49:35 +0900

    Update pymysql version information
    Set pymysql version info to 2.2.4

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,8 @@ import os
 from pathlib import Path
 import pymysql
 pymysql.install_as_MySQLdb()
+pymysql.version_info = (2, 2, 4, "final", 0)
+pymysql.__version__ = "2.2.4"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
