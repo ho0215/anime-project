3e687643ccd1fc8beafb99baeb6654230324a10a
ho0215 <ho021524@gmail.com>
2026-08-24 16:15:32 +0900

    Integrate django-environ for environment variable management
    Added support for environment variables using django-environ.

--- config/settings.py
+++ config/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
