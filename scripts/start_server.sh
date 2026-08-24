527b35314908314c0010ffe5cf430b9b590e5305
ho0215 <ho021524@gmail.com>
2026-08-24 09:32:29 +0900

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -e
 
-# 프로젝트 폴더로 이동
-cd /home/ec2-user/aniverse
+cd /home/ubuntu/aniverse
 
-# 가상환경 활성화
-source /home/ec2-user/venv/bin/activate
-
-# 기존에 돌고 있던 Gunicorn 프로세스 종료 (포트 충돌 방지)
+# 기존 구동 중이던 gunicorn 프로세스 종료
 pkill -f gunicorn || true
 
-# Gunicorn을 백그라운드로 실행 (에러는 server_error.log에 기록)
-nohup gunicorn --bind 0.0.0.0:8000 config.wsgi:application > /home/ec2-user/aniverse/server_error.log 2>&1 &
+# 가상환경 안의 gunicorn 절대 경로로 백그라운드 실행
+nohup /home/ubuntu/venv/bin/gunicorn --bind 0.0.0.0:8000 config.wsgi:application > /home/ubuntu/aniverse/server_error.log 2>&1 &
 
-echo "Django server started successfully in background."
+echo "Gunicorn server started successfully on Ubuntu."
